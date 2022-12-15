1007f007d13f50b62e452f35a7c60e93864a6732
PKGISREAL <114967470+PKGISREAL@users.noreply.github.com>
2022-12-15 22:14:13 +0800

    Update Problem 15.hs

--- Lab 2/Problem 15.hs
+++ Lab 2/Problem 15.hs
@@ -5,4 +5,4 @@ main = putStr (show (duplicate 3 [4,1,6,1]))
 
 duplicate :: Int -> [a] -> [a]
 duplicate _ [] = []
-duplicate n (x:xs) = (take n (cycle [x])) ++ dupli n xs
+duplicate n (x:xs) = (take n (cycle [x])) ++ duplicate n xs
